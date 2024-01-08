8147d22be08957b4163c8ed851739072f4664267
Liz FIschenich <Liz.Devlin85@gmail.com>
2024-01-08 14:55:51 -0600

    wireframe front end, started building out pages and components.

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
 import App from './App.jsx'
-import './index.css'
+import Home from './pages/home.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+    <App />,
+    errorElement: <Error />,
+    children:[
+      {
+        index: true,
+        element: <Home />,
+      },
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
