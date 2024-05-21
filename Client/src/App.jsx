a02a8be2e2f423ba0f08bdeed907d8e08b008da6
Liz FIschenich <Liz.Devlin85@gmail.com>
2024-05-20 20:51:51 -0500

    add cors and configured cors in server and front end

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,3 +1,42 @@
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import { Outlet } from 'react-router-dom';
+// import Nav from './components/nav.jsx';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+//   credentials:'include',
+// });
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   return (
+//       <ApolloProvider client={client}>
+//         <Nav />
+//         <Outlet />
+//       </ApolloProvider>
+//   );
+// }
+
+// export default App;
+
 import {
   ApolloClient,
   InMemoryCache,
@@ -9,8 +48,12 @@ import { Outlet } from 'react-router-dom';
 import Nav from './components/nav.jsx';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: process.env.NODE_ENV === 'production'
+    ? 'https://gamegameon-fda2e38a0db2.herokuapp.com/graphql'
+    : 'http://localhost:3001/graphql',
+  credentials: 'include', // Include credentials with requests
 });
+
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
@@ -20,6 +63,7 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,58 @@
+// const express = require('express');
+// const cors = require('cors');
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const corsOptions = {
+//   origin: 'https://gamegameon-fda2e38a0db2.herokuapp.com/', 
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+
+// // Create a new instance of an Apollo server with the GraphQL schema
+// const startApolloServer = async () => {
+//   await server.start();
+
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(express.json());
+
+//   app.use('/graphql', expressMiddleware(server, {
+//     context: authMiddleware
+//   }));
+
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//     });
+//   }
+
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// // Call the async function to start the server
+// startApolloServer();
+
 const express = require('express');
+const cors = require('cors');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
@@ -13,6 +67,17 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+// Determine CORS origin based on environment
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production'
+    ? 'https://gamegameon-fda2e38a0db2.herokuapp.com'
+    : 'http://localhost:3000',
+  credentials: true,
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
